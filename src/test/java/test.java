3774c11268f14013c361e109efd9cd02497d96b3
tianxb <952815417@qq.com>
2020-07-06 14:48:32 +0800

    测试

---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+import java.time.temporal.ValueRange;
+
+public class test {
+    public static void main(String[] args) {
+        long i =728001116951805952l;
+        for (int j = 0; j < 12; j++) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
+
+
+
+//宽表：10000000000381605
+//合同：879568
+//采购订单：10000000000176114
+//采购行项目：10000000000348349
