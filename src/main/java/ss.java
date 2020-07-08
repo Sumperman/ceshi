9547a5476ec7ac158c0bf4f7d5c6a055a76fb684
tianxb <952815417@qq.com>
2020-07-08 13:40:39 +0800

    测试1

--- src/main/java/ss.java
+++ src/main/java/ss.java
@@ -1,2 +1,7 @@
 public class ss {
+    public static void main(String[] args) {
+        String s ="a123";
+        boolean matches = s.matches("^[0-9]*$");
+        System.out.println(matches);
+    }
 }
